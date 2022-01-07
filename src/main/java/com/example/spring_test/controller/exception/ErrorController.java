ef1b7167d50cec903efcd53558562b20b1a01f38
Chia1104 <38397958+Chia1104@users.noreply.github.com>
2022-01-07 16:29:14 +0800

    coding error page

---
+++ src/main/java/com/example/spring_test/controller/exception/ErrorController.java
@@ -0,0 +1,13 @@
+package com.example.spring_test.controller.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "error";
+    }
+}
